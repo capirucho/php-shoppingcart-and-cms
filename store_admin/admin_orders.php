1ce87a67b94bba28ff5b8241325d71b2b75b5717
Ronald Figueroa <friedbeings@gmail.com>
2015-02-05 04:04:37 -0600

    orders, order details

--- store_admin/admin_orders.php
+++ store_admin/admin_orders.php
@@ -40,18 +40,20 @@ if ( !isUserLoggedIn() ) {
 
 			$orders_table = "shopcart_orders";
 			$command = "select * from ".$orders_table." order by order_id asc;";
-			$result = $db->query($command);		
+			$result = $db->query($command);	
 
-			while ( $data = $result->fetch_object() ) { 
+			while ( $data = $result->fetch_object() ) {
+
 				echo "<tr>";
-					echo "<td><a href=\"admin_orders.php?orderId=".$data->order_id."\" data-toggle=\"modal\" data-target=\"#orderDetails\" class=\"order-id\" >$data->order_id</a></td>";
+					echo "<td><a id=\"$data->order_id\" href=\"order_details.php?orderId=".$data->order_id."\" class=\"order-id\">$data->order_id</a></td>";
 					echo "<td>$data->order_status</td>";
 					echo "<td>$data->customer_id</td>";
 					echo "<td>$".$data->sub_total."</td>";
-					echo "<td>$data->tax</td>";
+					echo "<td>$data->tax%</td>";
 					echo "<td>$".$data->delivery_charge."</td>";
 					echo "<td>$".$data->total."</td>";
 				echo "</tr>";
+
 			}
 			$result->free();
 			$db->close();
@@ -59,40 +61,6 @@ if ( !isUserLoggedIn() ) {
 	</table>
 	
 </section>
-
-
-
-
-
-
-
-
-<div id="orderDetails" class="modal fade">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title">Details for Order: </h4>
-      </div>
-      <div class="modal-body">
-        
-	<?php
-
-		if ( isset($_GET['orderId']) ) {
-			$orderId = $_GET['orderId'];
-			echo "order id :".$orderId;
-
-		}
-
-	?>	
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
 
 
 <?php require 'footer.php'; ?>
--- store_admin/js/common.js
+++ store_admin/js/common.js
@@ -9,4 +9,24 @@ $(document).ready(function() {
 		//alert ('hello world');
 		//$('#orderDetails').modal('show');
 	//}
+
+
+/*
+   function OpenPopupCenter(pageURL, title, w, h) {
+        var left = (screen.width - w) / 2;
+        var top = (screen.height - h) / 4;  // for 25% - devide by 4  |  for 33% - devide by 3
+        var targetWin = window.open(pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    } 
+
+
+    $('.order-id').click(function(){
+    	var w = 400;
+    	var h = 400;
+    	var left = (screen.width - w) / 2;
+    	var top = (screen.height -h) / 4;
+    	window.open('order_details.php?orderId='+$(this).attr('id'), '_blank', 'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    return false;
+    });    
+*/
+
 });
